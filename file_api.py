a3aa96f6e1c21a057a60dc9683c10b638541e803
Mansoor Faqiri <mzfaqiri@gmail.com>
2024-05-20 14:07:01 -0400

    added some endpoint protection

--- file_api.py
+++ file_api.py
@@ -1,6 +1,7 @@
-from flask import Flask, redirect, send_from_directory, url_for
+from flask import Flask, redirect, send_from_directory, url_for,abort
 import json
 import os
+import re
 
 app = Flask(__name__)
 
@@ -9,9 +10,16 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route("/<file>")
+@app.route("/<file>", methods=["GET"])
 def file_api(file):
 
-    with open(os.path.join(app.root_path, "endpoints/"+file+".json"), "r") as data:
-        data = json.load(data)
-        return data
+    print(file)
+    pattern = re.compile(r'[A-Za-z]+')
+
+    if pattern.fullmatch(file):
+
+        with open(os.path.join(app.root_path, "endpoints/"+file+".json"), "r") as data:
+            data = json.load(data)
+            return data
+    else:
+        abort(404, 'invalid endpoint name')
